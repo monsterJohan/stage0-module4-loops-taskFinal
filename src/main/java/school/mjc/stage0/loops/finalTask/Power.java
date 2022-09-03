a12f7b22eba47598899c2ff787376b5e36e4c2ee
monsterJohan <nikfil97@gmail.com>
2022-09-03 15:09:20 +0200

    Implemented FinalTask

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,12 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        int number = numberToPrint;
+        for (int i = 1; i < power; i++) {
+            numberToPrint *= number;
+        }
 
+        System.out.println(numberToPrint);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,24 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            int number = i;
+            for (int j = 1; j <= cathetusLength * 2 - 1; j++) {
+                if (j <= cathetusLength - i) {
+                    System.out.print(" ");
+                } else if (j >= cathetusLength + i) {
+                    continue;
+                } else {
+                    System.out.print(number);
+                    if (j <= (cathetusLength * 2 - 1) / 2) {
+                        number--;
+                    } else {
+                        number++;
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
